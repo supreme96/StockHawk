ab427d7e72f52f50462cc0fd46751989db3835ae
Sahil Tandon <standon196@gmail.com>
2016-05-21 00:08:33 +0530

    Refractored Package Name

---
+++ app/src/main/java/com/sahil/android/stockhawk/data/QuoteDatabase.java
@@ -0,0 +1,16 @@
+package com.sahil.android.stockhawk.data;
+
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.Table;
+
+/**
+ * Created by sahil on 10/5/15.
+ */
+@Database(version = QuoteDatabase.VERSION)
+public class QuoteDatabase {
+  private QuoteDatabase(){}
+
+  public static final int VERSION = 7;
+
+  @Table(QuoteColumns.class) public static final String QUOTES = "quotes";
+}
---
+++ app/src/main/java/com/sahil/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,11 @@
+package com.sahil.android.stockhawk.touch_helper;
+
+/**
+ * Created by sahil on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface to enable swipe to delete
+ */
+public interface ItemTouchHelperAdapter {
+
+  void onItemDismiss(int position);
+}
